f6619b566fad84e676fc2c097c3a883819909850
huangzk@ihep.ac.cn <huangzk@ihep.ac.cn>
2026-04-29 12:50:03 +0800

    Remove unused legacy model backbones

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,9 +1,5 @@
-from .calodit import CaloDiT
-from .calodit_paper import CaloDiTPaper
 from .calodit_3drope import CaloLightningDiT
 
 __all__ = [
-    "CaloDiT",
-    "CaloDiTPaper",
     "CaloLightningDiT",
 ]
